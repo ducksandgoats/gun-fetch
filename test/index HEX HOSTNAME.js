d9af2928e547636e5a1fbcc3be463e465abbf621
resession <plainstud@gmail.com>
2021-11-01 20:54:01 -0500

    add test index file

--- test/index HEX HOSTNAME.js
+++ test/index HEX HOSTNAME.js
@@ -23,12 +23,91 @@ const LIST_OF_URLS = ["https://gun-manhattan.herokuapp.com/gun",
 "https://gun-armitro.herokuapp.com/",
 "https://fire-gun.herokuapp.com/gun"]
 
+/* 
+
+changes to be made
+
+
+have only one user, save the user data in a user file or a data folder which holds all the info
+
+_ is path
+@ is alias
+# is key
+for now, gun-fetch will have one user that holds the user credentials in a .env file or a file/folder
+
+GET is get
+PUT is set
+POST is put
+PATCH is unset
+DELETE is delete(put with null)
+
+a special case is a not query, potential way to do it can be a special character in front of the hostname or a special character in the end of the hostname, maybe an OPTIONS HTTP method can be added
+
+user/auth will be added later
+potential ways are
+(alias) or !alias for creating/destroying alias
+[alias] or $alias for adding and subtracting posts for a user that is currently logged in
+
+have one functionality per http method
+
+*/
+
 module.exports = function makeGunFetch(opts = null){
 
     const gun = Gun(opts || {peers: LIST_OF_URLS})
 
+    // let user = null
+
+    // if(!fs.existsSync('./data')){
+    //     fs.mkdirSync('./data')
+    //     fs.writeFileSync('./data/user.txt', crypto.randomBytes(8).toString('hex'))
+    //     fs.writeFileSync('./data/pass.txt', crypto.randomBytes(8).toString('hex'))
+    //     gun.user().create(fs.readFileSync('./data/user.txt').toString('utf-8'), fs.readFileSync('./data/pass.txt').toString('utf-8'), ack => {
+    //         if(ack.err){console.log(ack.err)} else {console.log(ack.pub)}
+    //         user = gun.user().auth(fs.readFileSync('./data/user.txt').toString('utf-8'), fs.readFileSync('./data/pass.txt').toString('utf-8'), authen => {
+    //             if(authen.err){console.log(authen.err)} else {console.log(authen.soul)}
+    //         })
+    //     })
+    // } else {
+    //     if(!fs.existsSync('./data/user.txt') || !fs.existsSync('./data/pass.txt')){
+    //         fs.writeFileSync('./data/user.txt', crypto.randomBytes(8).toString('hex'))
+    //         fs.writeFileSync('./data/pass.txt', crypto.randomBytes(8).toString('hex'))
+    //         gun.user().create(fs.readFileSync('./data/user.txt').toString('utf-8'), fs.readFileSync('./data/pass.txt').toString('utf-8'), ack => {
+    //             if(ack.err){console.log(ack.err)} else {console.log(ack.pub)}
+    //             user = gun.user().auth(fs.readFileSync('./data/user.txt').toString('utf-8'), fs.readFileSync('./data/pass.txt').toString('utf-8'), authen => {
+    //                 if(authen.err){console.log(authen.err)} else {console.log(authen.soul)}
+    //             })
+    //         })
+    //     } else {
+    //         user = gun.user().auth(fs.readFileSync('./data/user.txt').toString('utf-8'), fs.readFileSync('./data/pass.txt').toString('utf-8'), authen => {
+    //             if(authen.err){console.log(authen.err)} else {console.log(authen.soul)}
+    //         })
+    //     }
+    // }
+    
+    // // must find out if there is benefit with logging out before exiting before it is added
+    // // logout before exit
+    // process.on('exit', () => {
+    //     user.leave()
+    //     process.exit()
+    // });
+
+    // //catches ctrl+c event
+    // process.on('SIGINT', () => {
+    //     user.leave()
+    //     process.exit()
+    // });
+    
+    // //catches uncaught exceptions
+    // process.on('uncaughtException', () => {
+    //     user.leave()
+    //     process.exit()
+    // });
+    // // logout before exit
+
     const SUPPORTED_METHODS = ['GET', 'PUT', 'DELETE', 'POST', 'PATCH', 'OPTIONS']
-    const SUPPORTED_TYPES = ['_', '*', '-', '$', '!']
+    const SUPPORTED_TYPES = ['_', '*', '$', '.']
+    const SUPPORTED_ACTION = '!'
     const users = {}
 
     const fetch = makeFetch(async request => {
@@ -49,14 +128,35 @@ module.exports = function makeGunFetch(opts = null){
 
           try {
               let {hostname, pathname, protocol} = new URL(url)
-              hostname = decodeURIComponent(hostname)
+              if(hostname[0] === SUPPORTED_TYPES[3]){
+                  hostname = Buffer.from(hostname.slice(1), 'hex').toString()
+              }
 
-              if(protocol !== 'gun:' || !method || !SUPPORTED_METHODS.includes(method) || !hostname || !SUPPORTED_METHODS.includes(hostname)){
+              if((protocol !== 'gun:' || !method || !SUPPORTED_METHODS.includes(method)) || (!hostname || hostname.length < 3) || (!SUPPORTED_METHODS.includes(hostname[0]) && SUPPORTED_ACTION !== hostname[hostname.length - 1] && !/^[a-zA-Z0-9]+$/.test(hostname)) && !SUPPORTED_TYPES.includes(hostname[0]) && SUPPORTED_ACTION !== hostname[hostname.length - 1] && !/^[a-zA-Z0-9_*$!]+$/.test(hostname)){
                   console.log('something wrong with the query')
-                  return new Error('invalid query, must be a valid query')
+                  return new Error('invalid resource, must be a resource that is valid')
               }
 
               let req = formatReq(`${hostname}${pathname}`, method, protocol)
+            //   console.log(req, body)
+              let query = null
+              switch (req.queryType) {
+                case '_':
+                    query = req.multiple ? gun.user(req.host).path(req.path) : gun.user(req.host)
+                    break
+                case '*':
+                    query = req.multiple ? gun.get('~@' + req.host).path(req.path) : gun.get('~@' + req.host)
+                    break
+                case '$':
+                    query = req.multiple ? users[req.host].path(req.path) : users[req.host]
+                    break
+                case '!':
+                    query = req.host
+                    break
+                default:
+                    query = req.multiple ? gun.get(req.host).path(req.path) : gun.get(req.host)
+                    break
+              }
 
               let res = {statusCode: 0, headers: {}, data: null}
               switch (req.queryMethod) {
@@ -65,7 +165,7 @@ module.exports = function makeGunFetch(opts = null){
                     let mainData = null
                     if(req.mainQuery){
                         mainData = await new Promise((resolve) => {
-                            req.makeQuery.once(found => {resolve(found)})
+                            query.once(found => {resolve(found)})
                         })
                       res.statusCode = 200
                       res.headers = {}
@@ -75,10 +175,10 @@ module.exports = function makeGunFetch(opts = null){
                       }
                     } else {
                         mainData = null
-                        if(!users[req.makeQuery]){
+                        if(!users[query]){
                             mainData = {err: 'User is not logged in'}
                         } else {
-                            mainData = users[req.makeQuery]
+                            mainData = users[query]
                         }
 
                         res.statusCode = 200
@@ -95,7 +195,7 @@ module.exports = function makeGunFetch(opts = null){
                     let mainData = null
                     if(req.mainQuery){
                         mainData = await new Promise((resolve) => {
-                            req.makeQuery.put(body).once(found => {
+                            query.put(body).once(found => {
                                 resolve(found)
                             })
                         })
@@ -106,7 +206,7 @@ module.exports = function makeGunFetch(opts = null){
                             res.headers['Content-Type'] = 'application/json; charset=utf-8'
                         }
                     } else {
-                        if(users[req.makeQuery]){
+                        if(users[query]){
                             mainData = {err: 'User is currently logged in'}
                             res.statusCode = 400
                             res.headers = {}
@@ -115,9 +215,9 @@ module.exports = function makeGunFetch(opts = null){
                                 res.headers['Content-Type'] = 'application/json; charset=utf-8'
                             }
                         } else {
-                            users[req.makeQuery] = gun.user()
+                            users[query] = gun.user()
                             mainData = await new Promise((resolve) => {
-                                users[req.makeQuery].auth(query, body, ack => {
+                                users[query].auth(query, body, ack => {
                                     if(ack.err){
                                         resolve(ack)
                                     } else {
@@ -127,8 +227,8 @@ module.exports = function makeGunFetch(opts = null){
                             })
 
                             if(mainData.err){
-                                users[req.makeQuery].leave()
-                                delete users[req.makeQuery]
+                                users[query].leave()
+                                delete users[query]
                                 res.statusCode = 400
                                 res.headers = {}
                             } else {
@@ -149,7 +249,7 @@ module.exports = function makeGunFetch(opts = null){
                     let mainData = null
                     if(req.mainQuery){
                         mainData = await new Promise((resolve) => {
-                            req.makeQuery.unset(body).once(found => {resolve(found)})
+                            query.unset(body).once(found => {resolve(found)})
                         })
                         
                         res.statusCode = 200
@@ -178,7 +278,7 @@ module.exports = function makeGunFetch(opts = null){
                     let mainData = null
                     if(req.mainQuery){
                         mainData = await new Promise((resolve) => {
-                            req.makeQuery.set(body).once(found => {resolve(found)})
+                            query.set(body).once(found => {resolve(found)})
                         })
                         
                         res.statusCode = 200
@@ -212,7 +312,7 @@ module.exports = function makeGunFetch(opts = null){
                     let mainData = null
                     if(req.mainQuery){
                         mainData = await new Promise((resolve) => {
-                            req.makeQuery.put(body).once(found => {resolve(found)})
+                            query.put(body).once(found => {resolve(found)})
                         })
                         
                         res.statusCode = 200
@@ -222,7 +322,7 @@ module.exports = function makeGunFetch(opts = null){
                             res.headers['Content-Type'] = 'application/json; charset=utf-8'
                         }
                     } else {
-                        if(!users[req.makeQuery]){
+                        if(!users[query]){
                             mainData = {message: 'User is not logged in'}
                             res.statusCode = 400
                             res.headers = {}
@@ -231,8 +331,8 @@ module.exports = function makeGunFetch(opts = null){
                                 res.headers['Content-Type'] = 'application/json; charset=utf-8'
                             }
                         } else {
-                            users[req.makeQuery].leave()
-                            delete users[req.makeQuery]
+                            users[query].leave()
+                            delete users[query]
                             mainData = {message: 'User has been logged out'}
                             res.statusCode = 200
                             res.headers = {}
@@ -251,7 +351,7 @@ module.exports = function makeGunFetch(opts = null){
 
                         let mainData = await new Promise((resolve) => {
                             checkClear = setTimeout(() => {resolve({not: false, message: 'timed out, most likely this has data'})}, 5000)
-                            req.makeQuery.not(found => {
+                            query.not(found => {
                                 resolve({found, not: true, message: 'done, most likely this does not have data'})
                             })
                         })
@@ -265,7 +365,7 @@ module.exports = function makeGunFetch(opts = null){
                             res.headers['Content-Type'] = 'application/json; charset=utf-8'
                         }
                     } else {
-                        if(!users[req.makeQuery]){
+                        if(!users[query]){
                             mainData = {err: 'User is not logged in'}
                         } else {
                             mainData = {user: query + ' is logged in'}
@@ -302,39 +402,29 @@ module.exports = function makeGunFetch(opts = null){
 
     function formatReq(req, method, protocol){
         let path = req.split('/').filter(Boolean)
-        let queryType = path.shift()
+        let join = path.join('.')
         let count = path.length
         let multiple = count > 1 ? true : false
         let host = path.shift()
-        // path = path.map(data => {return decodeURIComponent(data.replace(/[^a-zA-Z0-9]/g, ''))}).join('.')
-        path = path.map(data => {return decodeURIComponent(data)}).join('.')
-        let makeQuery = null
+        let queryType = ''
+        if(SUPPORTED_TYPES.includes(host[0])){
+            queryType = host[0]
+        } else if(SUPPORTED_ACTION === host[host.length - 1]){
+            queryType = host[host.length - 1]
+        }
+        host = host.replace(queryType, '')
+        // split = split.map(data => {return data.replace(/[^a-zA-Z0-9]/g, '')})
+        // let path = split.length ? split.join('.') : null
+        path = path.map(data => {return data.replace(/[^a-zA-Z0-9]/g, '')}).join('.')
         let mainQuery = null
-        switch (queryType) {
-            case SUPPORTED_TYPES[0]:
-                makeQuery = multiple ? gun.get(host).path(path) : gun.get(host)
-                mainQuery = true
-                break
-            case SUPPORTED_TYPES[1]:
-                makeQuery = multiple ? gun.get('~@' + host).path(path) : gun.get('~@' + host)
-                mainQuery = true
-                break
-            case SUPPORTED_TYPES[2]:
-                makeQuery = multiple ? gun.get('~' + host).path(path) : gun.get('~' + host)
-                mainQuery = true
-                break
-            case SUPPORTED_TYPES[3]:
-                makeQuery = multiple ? users[host].path(path) : users[host]
-                mainQuery = true
-                break
-            case SUPPORTED_TYPES[4]:
-                makeQuery = host
-                mainQuery = false
-                break
+        if(queryType !== SUPPORTED_ACTION){
+            mainQuery = true
+        } else {
+            mainQuery = false
         }
         let queryMethod = method
         let queryProtocol = protocol
-        return {makeQuery, mainQuery, queryMethod, queryProtocol, queryType}
+        return {path, join, count, multiple, host, queryType, mainQuery, queryMethod, queryProtocol}
     }
 
     fetch.destroy = () => {
